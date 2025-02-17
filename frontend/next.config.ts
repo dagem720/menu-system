3af2842d988af974e609c6062b1d9c86869e4dc7
Dagmawi Gebremariam <dagem720@gmail.com>
2025-02-17 22:16:33 +0300

    fix bread crumbs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://188.245.43.115:3005", 
+      },
+    ];
+  },
   /* config options here */
   eslint:{
     ignoreDuringBuilds: true,
